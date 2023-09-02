db1a74bb898ca2df2ab351aca3bb1f1abadb8222
vishal <93590077+JaY072002@users.noreply.github.com>
2023-09-02 16:37:11 +0530

    bug fixes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>Home Page</>
+    <Link href={"/filter"} style={{ display: "flex", height: "100vh", justifyContent: "center", alignItems: "center", fontSize: "3rem", color: "black", fontWeight: "bold" }}>
+      Click here to start your quiz
+    </Link>
   )
 }
--- src/app/start/page.jsx
+++ src/app/start/page.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 const Page = () => {
     return (
         <div className="flex justify-center items-center min-h-screen card">
-            <Link href={"/quiz"} className="text-2xl border rounded p-5 cursor-pointer ">
-                Click Here To Start Your Quiz!!
+            <Link href={"/quiz"} className="text-2xl border rounded p-5 cursor-pointer" style={{ fontWeight: "bold", color: "black" }}>
+                Let's Gooooo
             </Link>
         </div>
     )
